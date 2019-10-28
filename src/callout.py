6d5c3cd518c9da6a659d3162277edd32ab8b2d3c
tssud <tssudhish@gmail.com>
2019-10-28 21:31:42 +0000

    Added two folders src and test/use_callout
    src will contain the package file callout.py
    test will contain the test script

---
+++ src/callout.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 28 21:26:20 2019
+
+@author: Sudhish Kumar
+"""
+
+def call_hello(name):
+    print("Hello " + str(name))
+    
+if __name__=='__main__':
+    call_hello(" Sudhish")
---
+++ test/use_callout.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+from callout import call_hello
+
+call_hello( "Calling from another file")
