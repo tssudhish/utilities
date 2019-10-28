a12aed41dc83218c8e33c8262acdc12009f9e82d
tssud <tssudhish@gmail.com>
2019-10-28 21:40:17 +0000

    Added default path to src using curdir of the file and os.pardir and "src" modified:   test/use_callout.py

--- test/use_callout.py
+++ test/use_callout.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import os, sys
+sys.path.append(os.path.join(os.path.dirname(__file__),os.pardir, "src"))
+
 from callout import call_hello
 
 call_hello( "Calling from another file")
